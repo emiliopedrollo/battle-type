359c428e87c1ea12aded44a92e760d27f7942cf3
Emilio B. Pedrollo <emiliopedrollo@gmail.com>
2016-10-07 23:01:50 -0300

    adicionado primeiro teste de allegro

---
+++ alphas/allegro1.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   allegro1.h
+ * Author: emilio
+ *
+ * Created on 7 de Outubro de 2016, 22:50
+ */
+
+#ifndef ALLEGRO1_H
+#define ALLEGRO1_H
+
+void show_screen();
+
+#endif /* ALLEGRO1_H */
+
