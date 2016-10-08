1b6d0af8eed8629edc8ebb667c428814cd49b297
emilio <emiliopedrollo@gmail.com>
2016-10-08 13:52:02 -0300

    Implentação do exemplo de network do Allegro

---
+++ alphas/allegro_samples/ex_enet_client.h
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ex_enet_client.h
+ * Author: root
+ *
+ * Created on 8 de Outubro de 2016, 13:19
+ */
+
+#ifndef EX_ENET_CLIENT_H
+#define EX_ENET_CLIENT_H
+
+int init_client(int port);
+
+#endif /* EX_ENET_CLIENT_H */
+
--- main.c
+++ main.c
@@ -13,14 +13,56 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
+#include <stdbool.h>
 #include "alphas/allegro1.h"
+#include "alphas/allegro_samples/ex_enet_server.h"
+#include "alphas/allegro_samples/ex_enet_client.h"
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-
-    show_screen();
+  
+    bool is_server = false;
+    int port = 0;
+    
+    int c;
+    static struct option long_options[] = {
+        {"server",  optional_argument, 0,  0 },
+        {"port",    required_argument, 0,  0 },
+        {0,         0,                 0,  0 }
+    };
+    
+    while (1){        
+        int option_index = 0;        
+        c = getopt_long(argc, argv, "",
+                 long_options, &option_index);
+        
+        if (c == -1)
+            break;
+        
+        switch (c) {
+            case 0:
+                if (long_options[option_index].name == "server") {
+                    is_server = true;
+                } else if (long_options[option_index].name == "port") {
+                    port = atoi(optarg);
+                }
+                break;
+            default:
+                printf("?? getopt returned character code 0%o ??\n", c);
+        }
+                
+    }
+    
+    if (is_server){
+        start_server(port);
+    } else {
+        init_client(port);
+    }
+        
+    //show_screen();
     return (EXIT_SUCCESS);
 }
 
