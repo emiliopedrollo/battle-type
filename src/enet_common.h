f97c9a14ace37a7d777f26bedef913ddff9a52aa
Emílio B. Pedrollo <emiliopedrollo@gmail.com>
2016-10-20 08:27:09 +0000

    Avanços no jogo

--- src/enet_common.h
+++ src/enet_common.h
@@ -5,6 +5,18 @@
 #ifndef BATTLE_TYPE_ENET_COMMON_H
 #define BATTLE_TYPE_ENET_COMMON_H
 
+#include "game.h"
+
 #define DEFAULT_PORT 9233
+
+typedef enum {
+    MESSAGE_TYPE_GAME_SNAPSHOP
+} MESSAGE_TYPE;
+
+
+typedef struct {
+    MESSAGE_TYPE type;
+    GAME_SNAPSHOT game;
+} SERVER_MESSAGE;
 
 #endif //BATTLE_TYPE_ENET_COMMON_H
