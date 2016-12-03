b7673df7c323bb36c4d54a291d0690375e12a8b6
fabio <fabioaraujopedroso>
2016-12-03 03:09:45 -0200

    Resolve #20 : Não há animações de lasers e/ou misseis

--- src/game.c
+++ src/game.c
@@ -34,7 +34,7 @@ BATTLESHIP *host_ships[NUMBER_OF_SHIPS_PER_PLAYER];
 BATTLESHIP *client_ships[NUMBER_OF_SHIPS_PER_PLAYER];
 BATTLESHIP *host_mothership;
 BATTLESHIP *client_mothership;
-ALLEGRO_BITMAP *rsc_explosion[16];
+
 char **dictionary;
 int dictionary_len;
 char host_target = -1, client_target = -1;
@@ -291,16 +291,16 @@ void move_game_ships() {
 void draw_game_ships() {
     for (int i = 0; i < NUMBER_OF_SHIPS_PER_PLAYER; i++) {
         if (is_multiplayer_client()) {
-            if (client_ships[i] && client_ships[i]->active) draw_ship(client_ships[i]);
+            if (client_ships[i] && client_ships[i]->active) draw_ship(client_ships[i],host_mothership->dx);
             if (host_ships[i] && host_ships[i]->active) {
-                draw_ship(host_ships[i]);
+                draw_ship(host_ships[i],client_mothership->dx);
                 if (!is_game_paused())
                     draw_ship_word(host_ships[i], false);
             }
         } else {
-            if (host_ships[i] && host_ships[i]->active) draw_ship(host_ships[i]);
+            if (host_ships[i] && host_ships[i]->active) draw_ship(host_ships[i],client_mothership->dx);
             if (client_ships[i] && client_ships[i]->active) {
-                draw_ship(client_ships[i]);
+                draw_ship(client_ships[i],host_mothership->dx);
                 if (!is_game_paused())
                     draw_ship_word(client_ships[i], false);
             }
@@ -329,8 +329,8 @@ void draw_game_ships() {
             break;
     }
 
-    draw_ship(host_mothership);
-    draw_ship(client_mothership);
+    draw_ship(host_mothership,client_mothership->dx);
+    draw_ship(client_mothership,host_mothership->dx);
 }
 
 void update_game_from_snapshot() {
--- src/menu_screen.c
+++ src/menu_screen.c
@@ -164,7 +164,7 @@ void init_demo_ship(){
 
 void draw_demo_ship(){
     move_ship(demo_ship,0);
-    draw_ship(demo_ship);
+    draw_ship(demo_ship,0);
 }
 
 char remote_ip[16] = "";
